3649279931d8ca0b47c8323416f1c7328c35890f
benjaminpolydeq <kbenjio9@gmail.com>
2026-01-11 23:45:43 +0000

    Delete chat.engine.py

--- chat.engine.py
+++
@@ -1,6 +0,0 @@
-def add_message(db, contact_id, content):
-    from models.message import Message
-    msg = Message(contact_id=contact_id, content=content)
-    db.add(msg)
-    db.commit()
-    return msg
