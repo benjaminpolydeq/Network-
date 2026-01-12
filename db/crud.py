6d1febfdca051cde8cf20e5e3d77a5034b305b93
benjaminpolydeq <kbenjio9@gmail.com>
2026-01-12 14:22:53 +0000

    Update crud.py

--- db/crud.py
+++ db/crud.py
@@ -1,20 +1,12 @@
 from sqlalchemy.orm import Session
-from . import models
+from .models import Contact
 
-def create_contact(db: Session, contact_data: dict):
-    contact = models.Contact(**contact_data)
+def create_contact(db: Session, data: dict):
+    contact = Contact(**data)
     db.add(contact)
     db.commit()
     db.refresh(contact)
     return contact
 
 def get_contacts(db: Session):
-    return db.query(models.Contact).all()
-
-def update_contact_importance(db: Session, contact_id: int, importance: int):
-    contact = db.query(models.Contact).filter(models.Contact.id == contact_id).first()
-    if contact:
-        contact.importance = importance
-        db.commit()
-        db.refresh(contact)
-    return contact
+    return db.query(Contact).order_by(Contact.created_at.desc()).all()
