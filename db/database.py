de019ae56f96eb4b38737e5e0aa240e6fc769d17
benjaminpolydeq <kbenjio9@gmail.com>
2026-01-12 14:22:07 +0000

    Update database.py

--- db/database.py
+++ db/database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
-DB_PATH = "sqlite:///network.db"
 
-engine = create_engine(DB_PATH, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    "sqlite:///network.db",
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(bind=engine)
 
 def init_db():
-    from . import models
+    from .models import Contact
     Base.metadata.create_all(bind=engine)
