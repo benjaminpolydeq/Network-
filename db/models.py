f7db8ac65de99b103fe4443da3aae489bda07463
benjaminpolydeq <kbenjio9@gmail.com>
2026-01-12 14:22:29 +0000

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,16 +1,16 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from datetime import datetime
 from .database import Base
 
 class Contact(Base):
     __tablename__ = "contacts"
-    id = Column(Integer, primary_key=True, index=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     contact_info = Column(String)
     domain = Column(String)
     occasion = Column(String)
     topics = Column(Text)
     next_action = Column(Text)
-    rendez_vous = Column(DateTime)
     importance = Column(Integer, default=0)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, default=datetime.utcnow)
